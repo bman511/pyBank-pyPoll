920f20c2d219c5d0e63cb6e0891f4fcb7bc52238
Brandon A <7830127+bman511@users.noreply.github.com>
2018-12-02 14:59:26 -0500

    updated script

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,7 +3,7 @@
 import csv
 
 total_votes = 0
-candidates = []
+candidates = {}
 
 with(open('election_data.csv',mode="r")) as results:
     el_results = csv.reader(results,delimiter=",")
@@ -11,9 +11,15 @@ with(open('election_data.csv',mode="r")) as results:
     for vote in el_results:
         total_votes += 1
         #check if candidate name in dictionary
+        if cadidates.get(row[2]):
+            curCan = candidates[row[2]]
+            totalVote = candidates[curCan] + 1
+            candidates[curCan] = totalVote
+        else:
+          candidates[row[2]] = 0  
           #if not, add them
           #if so, add vote for them
-
+    
     #calculate percentages and add to dictionary
     #get list of vote tallies, check for max
     #Find associated candidate who is the winner      
