b2a6335d2f8a84438160ccbd861496831873f429
Brandon A <7830127+bman511@users.noreply.github.com>
2018-12-03 21:49:35 -0500

    tally script is working

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,24 +5,27 @@ import csv
 total_votes = 0
 candidates = {}
 
-with(open('election_data.csv',mode="r")) as results:
+with(open('election_dataTest.csv',mode="r")) as results:
     el_results = csv.reader(results,delimiter=",")
-    next(el_results,None)
+    #next(el_results,None)
     for vote in el_results:
         total_votes += 1
-        #check if candidate name in dictionary
-        if cadidates.get(row[2]):
-            curCan = candidates[row[2]]
+        if candidates.get(vote[2]):
+            curCan = vote[2]
             totalVote = candidates[curCan] + 1
             candidates[curCan] = totalVote
         else:
-          candidates[row[2]] = 0  
+          candidates[vote[2]] = 1  
           #if not, add them
           #if so, add vote for them
     
     #calculate percentages and add to dictionary
     #get list of vote tallies, check for max
-    #Find associated candidate who is the winner      
+    #Find associated candidate who is the winner
+    
+for cand,voteTotal in candidates.items():
+    print(cand + " " + str(voteTotal) + "\n")
 print(total_votes)
 
+#print(len(candidates))
 
