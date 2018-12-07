f2b3148a771ba07b32b1929608a81fc0cacde089
Brandon A <7830127+bman511@users.noreply.github.com>
2018-12-07 14:51:00 -0500

    cleaned up comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,16 +7,7 @@ months_count = 0
 net_total = 0
 greatest_increase = [0,"month"]
 greatest_decrease = [0,"month"]
-"""
-with open('budget_data.csv', mode='r') as budget:
-    file_data = csv.reader(budget,delimiter = ',')
-    for row in file_data:
-        #print(int(row[1]))
-        months_count += 1
 
-
-print(months_count - 1 )
-"""
 with open('budget_data.csv', mode='r') as budget:
     file_data = csv.reader(budget,delimiter = ',')
     next(file_data,None)
@@ -43,13 +34,5 @@ with open('budget_data.csv', mode='r') as budget:
                 "-----------------" ]
     with open(theFileName,"w") as output:
         for string in resultsArr:
-            #print(string)
             printWrite(string,output)
             
-# print(str(months_count) + "\n")
-# print(str(net_total)+"\n")
-# average_change = net_total/months_count
-# print(f"{round(average_change,2)}\n")
-# print(f"{greatest_increase[0]} - {greatest_increase[1]}\n")
-# print(f"{greatest_decrease[0]} - {greatest_decrease[1]}\n")
-#newline='\n', optional for open(), no change if included
